9b50b7d6d0d4e668384979a59e0fa5a54ecb58f7
Tyler182 <msb7105@gmail.com>
2022-06-28 09:23:10 +0300

    Added 4 exercise

--- pandas/pandas_data_series.py
+++ pandas/pandas_data_series.py
@@ -93,14 +93,65 @@ def divide_series(series1, series2):
         Devision of series1 and series2
 
     """
+
     return series1/series2
 
 
+def compare_series(series1, series2):
+    """Compare elements of two series
+
+    4. Write a Pandas program to compare the elements of the two Pandas Series
+
+    Args:
+        series1 (pandas.Series): First series to compare.
+        series2 (pandas.Series): Second series to compare.
+    Returns:
+        Series of boolean, resulting compare elements of two series
+
+    """
+
+    if len(series1) == len(series2):
+        return series1 == series2
+    else:
+        return 'Could not compare because of different length of series'
+
+
 class TestPandasDataSeries(unittest.TestCase):
+
+    def test_compare_series_equel_length(self):
+        """Test - compare 2 series equel length
+        """
+
+        series1 = pd.Series([1, 2, 3])
+        series2 = pd.Series([2, 2, 'a'])
+        self.assertEqual(
+            list(compare_series(series1,series2)),
+            [False, True, False]
+            )
+
+    def test_compare_series_different_length(self):
+        """Test - compare 2 series different length
+        """
+
+        series1 = pd.Series([1, 2, 3, 4])
+        series2 = pd.Series([1, 2, 3])
+        self.assertEqual(
+            compare_series(series1,series2),
+            'Could not compare because of different length of series'
+            )
+
+    def test_compare_series_zero_length(self):
+        """Test - compare 2 series zero length
+        """
+
+        series1 = pd.Series([], dtype='float64')
+        series2 = pd.Series([], dtype='float64')
+        self.assertEqual(list(compare_series(series1,series2)),[])
+
     def test_add_series(self):
+        """Test add two Pandas Series
         """
-        Test add two Pandas Series
-        """
+
         series1 = pd.Series([1, 2, 3])
         series2 = pd.Series([1, 2, 3])
         self.assertEqual(
@@ -108,39 +159,39 @@ class TestPandasDataSeries(unittest.TestCase):
 
 
     def test_subtract_series(self):
+        """Test subtract two Pandas Series
         """
-        Test subtract two Pandas Series
-        """
-        series1 = pd.Series([1,2,3])
-        series2 = pd.Series([1,2,3])
+
+        series1 = pd.Series([1, 2, 3])
+        series2 = pd.Series([1, 2, 3])
         self.assertEqual(list(subtract_series(series1,series2)), [0, 0, 0])
 
 
     def test_multiple_series(self):
+        """Test multiple two Pandas Series
         """
-        Test multiple two Pandas Series
-        """
-        series1 = pd.Series([1,2,3])
-        series2 = pd.Series([1,2,3])
+
+        series1 = pd.Series([1, 2, 3])
+        series2 = pd.Series([1, 2, 3])
         self.assertEqual(
         list(multiple_series(series1,series2)), [1, 4, 9])
 
 
     def test_divide_series(self):
+        """Test divide two Pandas Series
         """
-        Test divide two Pandas Series
-        """
-        series1 = pd.Series([1,2,3])
-        series2 = pd.Series([1,2,3])
+
+        series1 = pd.Series([1, 2, 3])
+        series2 = pd.Series([1, 2, 3])
         self.assertEqual(list(divide_series(series1,series2)), [1, 1, 1])
 
 
     def test_series_to_list(self):
+        """Convert Series to List and compare with right List.
         """
-        Convert Series to List and compare with right List.
-        """
+
         series = pd.Series(['Yana','Ivan','Alex'], name='Children')
-        list_ = ['Yana','Ivan','Alex']
+        list_ = ['Yana', 'Ivan', 'Alex']
         self.assertEqual(series_to_list(series), list_)
 
 
