d9bfabd7cb00faf04eeec1a49f48d1c453e9c4ac
Tyler182 <msb7105@gmail.com>
2023-10-05 17:39:50 +0300

    Pandas #4 exercise

--- pandas/pandas_data_frame.py
+++ pandas/pandas_data_frame.py
@@ -40,6 +40,36 @@ def create_data_frame_with_index():
     print(df)
     return df
 
+def sum_inf_data_frame():
+    """3. Write a Pandas program to display a summary of the
+    basic information about a specified DataFrame and its data. 
+
+    Args:
+        -
+    Returns:
+        Pandas Data Frame
+
+    """
+    exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+                 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+                 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+                 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+    labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    df = pd.DataFrame(exam_data, labels)
+    return df.info()
+    
+def get_3_rows(df):
+    """Write a Pandas program to get the first 3 rows
+    of a given DataFrame. 
+
+    Args:
+        df(Pandas Data Frame) 
+    Returns:
+        Pandas Data Frame - 3 rows
+    """
+    
+    return df[0:3]    
+
 class TestPandasDataSeries(unittest.TestCase):
 
     def test_create_data_frame(self):
@@ -49,7 +79,13 @@ class TestPandasDataSeries(unittest.TestCase):
             )
 
 if __name__ == '__main__':
-    create_data_frame_with_index()
+    exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+                 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+                 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+                 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+    labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    df = pd.DataFrame(exam_data, labels)
+    print(get_3_rows(df))
     unittest.main()
     
     
